d4f0aca5bc75bbcd74c5777dc1989d5a01afd154
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2021-08-12 22:08:12 +0900

    before fix

---
+++ BadMain.py
@@ -0,0 +1,204 @@
+from flask import Flask, request, abort, render_template,redirect
+from linebot import (
+	LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+	InvalidSignatureError
+)
+from linebot.models import (
+	MessageEvent, TextMessage, TextSendMessage,
+	UnfollowEvent, FollowEvent
+)
+import os
+import json
+import gspread
+import oauth2client
+import linebot
+
+import BadPush
+
+#アクセスキーの取得
+app = Flask(__name__)
+
+#BOTの認証。Heroku環境で設定済み。
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/")
+def hello_world():
+	return "hello world!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+	signature = request.headers['X-Line-Signature']
+
+	# get request body as text
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
+
+	# handle webhook body
+	try:
+		handler.handle(body, signature)
+	except InvalidSignatureError:
+		abort(400)
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+	"""
+	友だち追加したときのイベント。
+	UsersDBにID、アクティビティを追加。
+	"""
+    UserID = event.source.user_id
+    line_bot_api.reply_message(event.reply_token,
+		[
+			TextSendMessage(text='minatoJBSC【公式】です。\n友達追加ありがとうございます!!'),
+			TextSendMessage(text='これからは練習の休み、遅刻の連絡はこのアカウントからお願いします。'),
+			TextSendMessage(text='本日の練習をお休みする場合は「休み」、遅れて参加の場合は「遅刻」と送信して下さい。')
+		]
+	)
+	
+
+    UserID = event.source.user_id
+	BadPush.add(UserID)
+	line_bot_api.reply_message(event.reply_token,
+		[
+			TextSendMessage(text='minatoJBSC【公式】です。\n友達追加ありがとうございます!!'),
+			TextSendMessage(text='これからは練習の休み、遅刻の連絡はこのアカウントからお願いします。'),
+			TextSendMessage(text='本日の練習をお休みする場合は「休み」、遅れて参加の場合は「遅刻」と送信して下さい。')
+		]
+	)
+
+@handler.add(UnfollowEvent)
+def handle_unfollow(event):
+	"""
+	ブロックされた時のイベント。
+	UsersDBのIDとアクティビティを削除。
+	"""
+	UserID = event.source.user_id
+	BadPush.remove(UserID)
+	
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+	"""
+	UserｓのActivityによって条件分岐。
+	noneQuestionがデフォルト→メニューを表示。
+	waitQestionが質問待ち状態→次に入力されたテキストが質問になる。
+	"""
+	UserID = event.source.user_id
+	text = event.message.text
+	activity = BadPush.checkActivity(UserID)
+
+	if activity == "初期状態":
+		if (text =="休み")or(text =="休")or(text =="やすみ"):
+			BadPush.changeActivity(UserID,"休み遅刻")
+			BadPush.changeStatus(UserID,"休み")
+			line_bot_api.reply_message(event.reply_token,
+				[
+					TextSendMessage(text='お休みですね。\n理由を選択して送信して下さい。\n「家庭都合」、「体調不良」、「怪我」、「その他」')
+				]
+			)
+		elif (text =="遅刻")or(text =="遅")or(text =="ちこく"):
+			BadPush.changeActivity(UserID,"休み遅刻")
+			BadPush.changeStatus(UserID,"遅刻")
+			line_bot_api.reply_message(event.reply_token,
+				[
+					TextSendMessage(text='遅刻ですね。\n到着予定時間(見込)を送信して下さい。')
+				]
+			)
+		else:
+			line_bot_api.reply_message(event.reply_token,
+				[
+					TextSendMessage(text='申し訳ありませんその言葉は理解しかねます。\n「休み」もしくは「遅刻」と送信して下さい。')
+				]
+			)
+	elif activity == "休み遅刻":
+		ChildStat = BadPush.getStatus(UserID)
+		if (ChildStat =="休み"):
+			if (text =="家庭都合")or(text =="体調不良")or(text =="怪我")or(text =="その他"):
+				BadPush.changeActivity(UserID,"選手名")
+				BadPush.changeReason(UserID,text)
+				line_bot_api.reply_message(event.reply_token,
+					[
+						TextSendMessage(text='了解しました。\nお子さんの名前をフルネームで送信して下さい。')
+					]
+				)
+			else:
+				line_bot_api.reply_message(event.reply_token,
+					[
+						TextSendMessage(text='お休みの理由を選択して送信して下さい。\n「家庭都合」、「体調不良」、「怪我」、「その他」')
+					]
+				)
+		elif (ChildStat =="遅刻"):
+			BadPush.changeActivity(UserID,"選手名")
+			BadPush.changeReason(UserID,text)
+			line_bot_api.reply_message(event.reply_token,
+					[
+						TextSendMessage(text='了解しました。\nお子さんの名前をフルネームで送信して下さい。')
+					]
+				)
+	elif activity == "選手名":
+		BadPush.changePlayer(UserID,text)
+		BadPush.changeActivity(UserID,"補足")
+		line_bot_api.reply_message(event.reply_token,
+			[
+				TextSendMessage(text='その他補足事項等があれば入力し送信して下さい。\n特になければ「なし」で送信して下さい。')
+			]
+		)
+	elif activity == "補足":
+		BadPush.changeRemarks(UserID,text)
+		BadPush.changeActivity(UserID,"最終状態")
+		Status_text = BadPush.getStatus(UserID)
+		Reason_text = BadPush.getReason(UserID)
+		Player_text = BadPush.getPlayer(UserID)
+		Remarks_text = BadPush.getRemarks(UserID)
+		line_bot_api.reply_message(event.reply_token,
+			[
+				TextSendMessage(text=Player_text+"\n"+Status_text+":"+Reason_text+"\n"+Remarks_text),
+				TextSendMessage(text="上記で登録します。よろしければ「はい」を、訂正がある場合は「いいえ」を送信して下さい。")
+			]
+		)
+	elif activity =="最終状態":
+		if ( text == 'はい'):
+			BadPush.changeActivity(UserID,"初期状態")
+			Status_text = BadPush.getStatus(UserID)
+			Reason_text = BadPush.getReason(UserID)
+			Player_text = BadPush.getPlayer(UserID)
+			Remarks_text = BadPush.getRemarks(UserID)
+#			to = ["Ue7d160f7532b20d2336def091f82c0cb","U3027818607ab0c92984e92143dc06c58"]
+			to = ["Ue7d160f7532b20d2336def091f82c0cb", "U3027818607ab0c92984e92143dc06c58"]
+
+			messages = TextSendMessage(text=Player_text+"\n"+Status_text+":"+Reason_text+"\n"+Remarks_text)
+			line_bot_api.multicast(to, messages=messages)
+			line_bot_api.reply_message(event.reply_token,
+				[
+					TextSendMessage(text = '連絡を受け付けました。ありがとうございました。'),
+					TextSendMessage(text='本日の練習をお休みする場合は「休み」、遅れて参加の場合は「遅刻」と送信して下さい。')
+				]
+			)
+		elif (text == 'いいえ'):
+			BadPush.changeActivity(UserID,"初期状態")
+			line_bot_api.reply_message(event.reply_token,
+				[
+					TextSendMessage(text = 'お手数ですが初めからやり直して下さい。'),
+					TextSendMessage(text='本日の練習をお休みする場合は「休み」、遅れて参加の場合は「遅刻」と送信して下さい。')
+				]
+			)
+		else:
+			Status_text = BadPush.getStatus(UserID)
+			Reason_text = BadPush.getReason(UserID)
+			Player_text = BadPush.getPlayer(UserID)
+			Remarks_text = BadPush.getRemarks(UserID)
+			line_bot_api.reply_message(event.reply_token,
+				[
+					TextSendMessage(text=Player_text+"\n"+Status_text+":"+Reason_text+"\n"+Remarks_text),
+					TextSendMessage(text="上記で登録します。よろしければ「はい」を、訂正がある場合は「いいえ」を送信して下さい。")
+				]
+			)
+
+if __name__ == "__main__":
+	port = int(os.getenv("PORT",5000))
+	app.run(host="0.0.0.0", port=port)
---
+++ BadPush.py
@@ -0,0 +1,124 @@
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+import json
+import random
+
+scopes = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+json_file = 'spreadsheet-test-290912-d4a6f7917298.json'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file, scopes=scopes)
+
+# スプレッドシート用クライアントの準備
+client = gspread.authorize(credentials)
+SPREADSHEET_KEY = '1Y0v_qJqS52h5Tw5MyRRgnT8MDPog3qO7cUz26Q-j-gk'
+worksheet = client.open_by_key(SPREADSHEET_KEY).sheet1
+
+def add(userID):
+	num = worksheet.cell(1,1).value
+	num = int(num)
+		try:
+			worksheet.update_cell(num+1, 1, str(num))
+			worksheet.update_cell(num+1, 2, userID)
+			worksheet.update_cell(num+1, 3, "初期状態")
+			worksheet.update_cell(num+1, 4, "休み/遅刻")
+			worksheet.update_cell(num+1, 5, "理由/時間")
+			worksheet.update_cell(num+1, 6, "選手名")
+			worksheet.update_cell(num+1, 7, "備考")
+			worksheet.update_cell(1, 1, str(num+1))
+		except Exception as e:	# 例外発生時は0～4秒の時間待ち。同期ずらし。
+			ran = random.randrange(0,4)
+			sleep(ran)
+		else:					# 例外が発生しなかった場合、for を break
+			break
+
+def remove(UserID):
+	num = worksheet.cell(1,1).value
+	num = int(num)
+	cell = worksheet.find(UserID)
+	row = cell.row
+	if row != 0:
+			try:
+				worksheet.delete_row(row)
+			except Exception as e:	# 例外発生時は0～4秒の時間待ち。同期ずらし。
+				ran = random.randrange(0,4)
+				sleep(ran)
+			else:					# 例外が発生しなかった場合、for を break
+				break
+def checkActivity(UserID):
+	cell = worksheet.find(UserID)
+	activity = worksheet.cell(cell.row, 3).value
+	return activity
+
+def changeActivity(UserID,activity):
+	cell = worksheet.find(UserID)
+	for _ in range(10):			# 最大10回実行
+		try:
+			worksheet.update_cell(cell.row, 3, activity)
+		except Exception as e:	# 例外発生時は0～4秒の時間待ち。同期ずらし。
+			ran = random.randrange(0,4)
+			sleep(ran)
+		else:					# 例外が発生しなかった場合、for を break
+			break
+
+def changeStatus(UserID,ChildStatus):
+	cell = worksheet.find(UserID)
+	for _ in range(10):			# 最大10回実行
+		try:
+			worksheet.update_cell(cell.row, 4, ChildStatus)
+		except Exception as e:	# 例外発生時は0～4秒の時間待ち。同期ずらし。
+			ran = random.randrange(0,4)
+			sleep(ran)
+		else:					# 例外が発生しなかった場合、for を break
+			break
+
+def getStatus(UserID):
+	cell = worksheet.find(UserID)
+	ChildStatus = worksheet.cell(cell.row, 4).value
+	return ChildStatus
+
+def changeReason(UserID,Reason):
+	cell = worksheet.find(UserID)
+	for i in range(10):			# 最大10回実行
+		try:
+			worksheet.update_cell(cell.row, 5, Reason)
+		except Exception as e:	# 例外発生時は0～4秒の時間待ち。同期ずらし。
+			ran = random.randrange(0,4)
+			sleep(ran)
+		else:					# 例外が発生しなかった場合、for を break
+			break
+
+def getReason(UserID):
+	cell = worksheet.find(UserID)
+	Reason = worksheet.cell(cell.row, 5).value
+	return Reason
+
+def changePlayer(UserID,PlayerName):
+	cell = worksheet.find(UserID)
+	for _ in range(10):			# 最大10回実行
+		try:
+			worksheet.update_cell(cell.row, 6, PlayerName)
+		except Exception as e:	# 例外発生時は0～4秒の時間待ち。同期ずらし。
+			ran = random.randrange(0,4)
+			sleep(ran)
+		else:					# 例外が発生しなかった場合、for を break
+			break
+
+def getPlayer(UserID):
+	cell = worksheet.find(UserID)
+	PlayerName = worksheet.cell(cell.row, 6).value
+	return PlayerName
+
+def changeRemarks(UserID,Remarks):
+	cell = worksheet.find(UserID)
+	for i in range(10):			# 最大10回実行
+		try:
+			worksheet.update_cell(cell.row, 7, Remarks)
+		except Exception as e:	# 例外発生時は0～4秒の時間待ち。同期ずらし。
+			ran = random.randrange(0,4)
+			sleep(ran)
+		else:					# 例外が発生しなかった場合、for を break
+			break
+
+def getRemarks(UserID):
+	cell = worksheet.find(UserID)
+	Remarks = worksheet.cell(cell.row, 7).value
+	return Remarks
