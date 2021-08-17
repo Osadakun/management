cd5a585ecbfd2c8272ff17877902611e0d6c9a40
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2021-08-17 21:54:47 +0900

    refix

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+db = ""
+
 #herokuへのデプロイが成功したかどうかを確認するためのコード
 @app.route("/")
 def hello_world():
