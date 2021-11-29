776b75e00da4d87f6ff90f5d8bf72605bdcbb977
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2021-11-29 11:35:42 +0900

    fix

--- BadPush.py
+++ BadPush.py
@@ -4,45 +4,37 @@ import json
 import random
 
 scopes = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-json_file = 'spreadsheet-test-290912-d4a6f7917298.json'
+json_file = 'linebot.json'
 credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file, scopes=scopes)
 
 # スプレッドシート用クライアントの準備
 client = gspread.authorize(credentials)
-SPREADSHEET_KEY = '1Y0v_qJqS52h5Tw5MyRRgnT8MDPog3qO7cUz26Q-j-gk'
+SPREADSHEET_KEY = '9b898ec51baca23f18443c68f29b392b7d9a6b60'
 worksheet = client.open_by_key(SPREADSHEET_KEY).sheet1
 
 def add(userID):
 	num = worksheet.cell(1,1).value
 	num = int(num)
-		try:
-			worksheet.update_cell(num+1, 1, str(num))
-			worksheet.update_cell(num+1, 2, userID)
-			worksheet.update_cell(num+1, 3, "初期状態")
-			worksheet.update_cell(num+1, 4, "休み/遅刻")
-			worksheet.update_cell(num+1, 5, "理由/時間")
-			worksheet.update_cell(num+1, 6, "選手名")
-			worksheet.update_cell(num+1, 7, "備考")
-			worksheet.update_cell(1, 1, str(num+1))
-		except Exception as e:	# 例外発生時は0～4秒の時間待ち。同期ずらし。
-			ran = random.randrange(0,4)
-			sleep(ran)
-		else:					# 例外が発生しなかった場合、for を break
-			break
+	try:
+		worksheet.update_cell(num+1, 1, str(num))
+		worksheet.update_cell(num+1, 2, userID)
+		worksheet.update_cell(num+1, 3, "初期状態")
+		worksheet.update_cell(num+1, 4, "休み/遅刻")
+		worksheet.update_cell(num+1, 5, "理由/時間")
+		worksheet.update_cell(num+1, 6, "選手名")
+		worksheet.update_cell(num+1, 7, "備考")
+		worksheet.update_cell(1, 1, str(num+1))
+	except Exception as e:	# 例外発生時は0～4秒の時間待ち。同期ずらし。
+		ran = random.randrange(0,4)
+		sleep(ran)
+
 
 def remove(UserID):
 	num = worksheet.cell(1,1).value
 	num = int(num)
 	cell = worksheet.find(UserID)
 	row = cell.row
-	if row != 0:
-			try:
-				worksheet.delete_row(row)
-			except Exception as e:	# 例外発生時は0～4秒の時間待ち。同期ずらし。
-				ran = random.randrange(0,4)
-				sleep(ran)
-			else:					# 例外が発生しなかった場合、for を break
-				break
+
 def checkActivity(UserID):
 	cell = worksheet.find(UserID)
 	activity = worksheet.cell(cell.row, 3).value
